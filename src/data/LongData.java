22d98118421915be5d31614ee1d409957c5680eb
Oleksandr Kukurik <okukurik@griddynamics.mail>
2022-06-04 23:30:58 +0300

    outdated final

--- src/data/LongData.java
+++ src/data/LongData.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LongData extends Data<Long> {
@@ -13,24 +14,21 @@ public class LongData extends Data<Long> {
         super(input);
     }
 
-    public void mapInvalid(List<String> data) {
-        data
-                .stream()
-                .flatMap(s -> Arrays.stream(s.split(" ")))
-                .filter(x -> x.matches(".*\\D.*"))
-                .map(s -> String.format("\"%s\" is not a long. It will be skipped.\n", s))
-                .forEach(System.out::print);
-    }
-
     @Override
     public List<Long> mapData(List<String> data) {
-        mapInvalid(data);
         return data
                 .stream()
                 .map(String::trim)
                 .flatMap(s -> Arrays.stream(s.split(" ")))
-                .filter(x -> x.matches("-?\\d+(\\.\\d+)?"))
-                .map(Long::parseLong)
+                .map(x -> {
+                    try {
+                        return Long.parseLong(x);
+                    } catch (NumberFormatException e) {
+                        System.out.printf("\"%s\" is not a long. It will be skipped.\n", x);
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
                 .sorted(Comparator.naturalOrder())
                 .collect(Collectors.toList());
     }
